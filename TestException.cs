5ccc928fb0fbf3b9a1cb72444bc83f12ab10eff8
Abhishekpatil1168 <69276972+Abhishekpatil1168@users.noreply.github.com>
2021-10-19 00:46:11 +0530

    Add files via upload

---
+++ TestException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace HappyFlight
+{
+    [Serializable]
+    internal class TestException : Exception
+    {
+        public TestException()
+        {
+        }
+
+        public TestException(string message) : base(message)
+        {
+        }
+
+        public TestException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TestException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
